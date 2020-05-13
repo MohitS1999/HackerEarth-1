2aab3e5cf2d471cf3228d246700449f97c532c38
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-05-14 01:39:57 +0530

    Update BinaryAssociative.java

--- BinaryAssociative.java
+++ BinaryAssociative.java
@@ -14,6 +14,17 @@ public class BinaryAssociative{
 			 c2=sc.nextInt();
 			 c3=sc.nextInt();
 			 int flag=0;
+			//Loop for table
+			/*
+			i j k
+			0 0 0
+			0 1 0
+			0 1 1
+			1 0 0
+			1 0 1
+			1 1 0
+			1 1 1
+			*/
 			 for (int i=0;i<2;i++){
 				 for(int j=0;j<2;j++){
 					 for (int k=0;k<2;k++){
@@ -35,7 +46,7 @@ public class BinaryAssociative{
 			
 		}
 	}
-	
+	//Truth Table
 	static int fun(int a,int b){
 		if (a==0 && b==0)
 			return c0;
