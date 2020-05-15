255d7e5d14822e6ca84681f4bc81c9f7b282d6d1
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-05-16 01:15:22 +0530

    Dice

---
+++ Dice.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Dice{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		int len=s.length();
+		int cnt=0;
+		int i=0;
+		int flag=0;
+		for(char c:s.toCharArray()){
+			i++;
+			if (c!='6')
+				cnt++;
+			//System.out.println("i"+i);
+			if (i==len){
+				if (c=='6')
+					flag=1;
+			}
+		}
+		if (flag==0)
+			System.out.println(cnt);
+		else 
+			System.out.println("-1");
+	}
+}
