41c926c9568b690d1f51ae492b4cb9b6a73255d8
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-05-19 00:10:43 +0530

    GreatKian

---
+++ GreatKian.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class GreatKian{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+			arr[i]=sc.nextInt();
+		for(int i=0;i<3;i++){
+			long sum=0;
+			for(int j=i;j<n;j=j+3){
+				sum+=arr[j];
+			}
+			System.out.print(sum+" ");
+		}
+	}
+}
