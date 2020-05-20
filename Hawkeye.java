78552cff4c19a5c20ef9358119ca15d97977e6d8
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-05-20 10:42:03 +0530

    Basic Implementation

---
+++ Hawkeye.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Hawkeye{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[][]=new int[n][n];
+		//Arrays.fill(arr,0);
+		int i=sc.nextInt();
+		int j=sc.nextInt();
+		int p=sc.nextInt();
+		// for(int x=0;x<n;x++){
+			// for(int y=0;y<n;y++){
+				// arr[x][y]=0;
+			// }
+		// }
+		arr[i][j]=p;
+		p--;
+		for(int k=1;k<n;k++){
+			
+		//	System.out.println("p---"+p);
+			for(int x=i-k;x<=i+k;x++){
+				for(int y=j-k;y<=j+k;y++){
+					//System.out.println(x+"   "+y);
+					if(x>-1 && y>-1 && x<n && y<n && arr[x][y]==0)
+						arr[x][y]=p;
+				}
+			}
+			if (p>0)
+				p--;
+		}
+		for(int x=0;x<n;x++){
+			for(int y=0;y<n;y++){
+				System.out.print(arr[x][y]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
