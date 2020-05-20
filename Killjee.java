341d1ee3041f60ca38fdba765da61574ef88d0a2
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-05-21 00:41:17 +0530

    Basic Implementation

--- Killjee.java
+++ Killjee.java
@@ -1,4 +1,5 @@
 //https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/approximate/killjee-and-easy-problem/
+//Dfs
 import java.util.*;
 public class Killjee{
 	static int n;
