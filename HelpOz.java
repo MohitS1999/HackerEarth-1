fdbd931524ca755b42886d22e886cb3463d6795e
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-06-07 00:43:10 +0530

    Basic Math

---
+++ HelpOz.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class HelpOz{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt();
+		int ar[]=new int[m];
+		int max=Integer.MIN_VALUE;
+		for (int i=0;i<m;i++){
+			int n=sc.nextInt();
+			ar[i]=n;
+			if (max<n)
+				max=n;
+		}
+		//System.out.println(min);
+		for (int k=2;k<=max;k++){
+			boolean flag=true;
+			for (int i=0;i<m-1;i++){
+				if (ar[i]%k!=ar[i+1]%k)
+				{
+					flag=false;
+					break;
+				}
+			}
+			if (flag)
+				System.out.print(k+" ");
+		}
+	}
+}
