0ccd83f0be56fc491a7e32a81388e502a2400892
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-06-13 10:26:36 +0530

    Basic Math

--- PandaAndChain.java
+++ PandaAndChain.java
@@ -13,6 +13,7 @@ import java.util.*;
 			while(t-->0){
 				long n=sc.nextLong();
 				long x=sc.nextLong();
+				//if n is greater than mod then print 1 bcoz after mod always comes 0
 				if (n>mod)
 					System.out.println("0");
 				else
