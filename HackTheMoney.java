0ea868db08c2ac8a49ab2fcb7d33e3297e74e3cc
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-06-18 00:18:15 +0530

    Recursion

---
+++ HackTheMoney.java
@@ -0,0 +1,33 @@
+//https://www.hackerearth.com/practice/basic-programming/recursion/recursion-and-backtracking/practice-problems/algorithm/hack-the-money/submissions/
+import java.util.*;
+public class HackTheMoney{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            float n=sc.nextFloat();
+            if (n==1)
+                System.out.println("Yes");
+            else if (n<10)
+                System.out.println("No");
+            else{
+                if (canHack(n)){
+                    System.out.println("Yes");
+                }else{
+                    System.out.println("No");
+                }
+            }
+        }
+    }
+    static boolean canHack(float n){
+		//System.out.println("n:- "+n);
+        if ((n-((int)n)!=0) || (n<10 && n>1))
+            return false;
+        if (n==1)
+            return true;
+        if (canHack(n/10)||(canHack(n/20)))
+            return true;
+        else
+            return false;
+    }
+}
