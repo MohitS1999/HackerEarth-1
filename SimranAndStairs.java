2f1e39e5e0c3c3b70b1b81e0aef1bef08d0dee68
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-06-23 17:37:06 +0530

    Recursion

---
+++ SimranAndStairs.java
@@ -0,0 +1,18 @@
+//https://www.hackerearth.com/practice/basic-programming/recursion/recursion-and-backtracking/practice-problems/algorithm/simran-and-stairs/
+import java.util.*;
+public class SimranAndStairs{
+	static int ans;
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		ans=0;
+		System.out.println(stairs(n));
+	}
+	public static int stairs(int n){
+		if (n==0 || n==1)
+			return 1;
+		else if (n==2)
+			return 2;
+		return stairs(n-1)+stairs(n-2)+stairs(n-3);
+	}
+}
